2b3e11b1ecb4b0bde04489390caddba630e5b24f
Tyler Compton <xaviosx@gmail.com>
2021-11-01 11:01:25 -0700

    Re-use ports

--- debutizer/commands/local_repo.py
+++ debutizer/commands/local_repo.py
@@ -27,6 +27,7 @@ class LocalRepository:
             app=self._app,
             handler_class=simple_server.WSGIRequestHandler,
         )
+        self._server.allow_reuse_address = True
 
         self._thread = Thread(
             name="Local Repository",
