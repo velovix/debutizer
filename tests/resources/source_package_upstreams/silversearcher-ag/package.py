fbf0f098e11b0b6401dcfdbc8143fe1068b529d5
Tyler Compton <xaviosx@gmail.com>
2022-06-17 14:01:09 -0700

    Update silversearcher test version

--- tests/resources/source_package_upstreams/silversearcher-ag/package.py
+++ tests/resources/source_package_upstreams/silversearcher-ag/package.py
@@ -11,8 +11,8 @@ def create_source_package(env: Environment) -> SourcePackage:
     upstream = SourcePackageUpstream(
         env=env,
         name="silversearcher-ag",
-        version=Version.from_string("2.2.0-1"),
-        dsc_url="http://archive.ubuntu.com/ubuntu/pool/universe/s/silversearcher-ag/silversearcher-ag_2.2.0-1.dsc",
+        version=Version.from_string("2.2.0+git20200805-1"),
+        dsc_url="http://archive.ubuntu.com/ubuntu/pool/universe/s/silversearcher-ag/silversearcher-ag_2.2.0+git20200805-1.dsc",
     )
     package_dir = upstream.fetch()
 
@@ -20,7 +20,7 @@ def create_source_package(env: Environment) -> SourcePackage:
 
     source_package.changelog.add(
         ChangeBlock(
-            version="2.2.0-1myorg1",
+            version="2.2.0+git20200805-1myorg1",
             urgency="medium",
             changes=[
                 "* Repackaged this in my repository!",
